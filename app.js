0c4bedd71a19912b413a595f134c0b66cc898e5d
neilmanuell <neil.revisual@gmail.com>
2015-01-15 23:02:51 +0000

    update express to 4

--- app.js
+++ app.js
@@ -1,39 +1,68 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-   , http = require('http')
-   , path = require('path');
+var express = require( 'express' )
+   , bodyParser = require( 'body-parser' )
+   , favicon = require( 'serve-favicon' )
+   , logger = require( 'morgan' )
+   , http = require( 'http' )
+   , path = require( 'path' )
+   , routes = require( './routes' );
 
 var PUBLIC_PATH = path.join( __dirname, 'public/' );
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set( 'port', process.env.PORT || 3000 );
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use( logger( 'dev' ) );
+app.use( bodyParser.urlencoded( {extended: false} ) );
+app.use( bodyParser.json() );
+/*app.use( express.methodOverride() );*/
 app.use( express.static( PUBLIC_PATH ) );
+app.use('/', routes);
 
 
-// development only
-if ('development' == app.get('env')) {
-   app.use(express.errorHandler());
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+   var err = new Error('Not Found');
+   err.status = 404;
+   next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+   app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+         message: err.message,
+         error: err
+      });
+   });
 }
 
-require('./routes')(app);
-
-
-
-http.createServer(app).listen(app.get('port'), function(){
-   console.log('Express server listening on port ' + app.get('port'));
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+   res.status(err.status || 500);
+   res.render('error', {
+      message: err.message,
+      error: {}
+   });
 });
+
+
+
+
+
+http.createServer( app ).listen( app.get( 'port' ), function () {
+   console.log( 'Express server listening on port ' + app.get( 'port' ) );
+} );
 
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,11 @@
-var api = require( './api' ) ,
-   home = require( './home' );
+var router = require( 'express' ).Router()
+   , api = require( './api' )
+   , home = require( './home' );
 
+router.get( '/', home.index );
+router.get( '/app/', home.app );
+router.get( '/api/projects/', api.project );
+router.get( '/api/:project/:book/:size', api.book );
+// app.post( '/api/upload/:name', api.upload, api.uploadSuccess, api.uploadError );
 
-
-module.exports = function ( app ) {
-
-  /* app.get('/', function (req, res){
-      res.writeHead(200, {'Content-Type': 'text/html' });
-      *//* Display the file upload form. *//*
-      var form = '<form action="/api/upload/good" enctype="multipart/form-data" method="post"><input name="title" type="text" /><input multiple="multiple" name="upload" type="file" /><input type="submit" value="Upload" /></form>';
-      res.end(form);
-   });*/
-
-   app.get( '/', home.index );
-   app.get( '/app/', home.app );
-   app.get( '/api/projects/', api.project );
-   app.get( '/api/:project/:book/:size', api.book );
-  // app.post( '/api/upload/:name', api.upload, api.uploadSuccess, api.uploadError );
-};
+module.exports = router;
